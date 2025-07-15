752b71801d27c30f0d1ed39bc2f902f39af2d0e8
sajti <sajti@vbox.home>
2025-07-15 14:03:13 +0200

    Modified

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { Pool } = require('pg');
+const fs = require('fs');
+const path = require('path');
+
+const pool = new Pool({
+    user: 'reader',
+    host: 'localhost',
+    database: 'librarydb',
+    password: 'reader',
+    port: 5432,
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
